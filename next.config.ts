e50d2cd296ca464a131cf47ca42caa447a65e1eb
Cevin Agustian <cevin.agust@gmail.com>
2026-05-07 00:48:01 +0700

    Hapus config lokal buat deploy Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    allowedDevOrigins: ['cold-pants-beam.loca.lt'],
-  },
+  /* config options di sini dikosongin aja atau biarin default */
 };
 
 export default nextConfig;
